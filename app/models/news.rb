79bdc262804b87ee17090cc02b3877b764cf07d7
drapes <tihon1997@gmail.com>
2017-07-25 00:30:30 +0300

    Валидации для моделей на первое время

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,2 +1,14 @@
 class News < ApplicationRecord
+  belongs_to :user
+
+  validates :datetime, :сontent, :title, :sources, :user, presence: true, on: :create
+  # TODO validates :datetime, :сontent, :sources, uniqueness: true
+
+  before_save :fill_in_date
+
+  private
+
+  def fill_in_date
+    self.date = datetime.to_date
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_many :news
+
+  validates :username, :password, presence: true
+  validates :username, uniqueness: true
+
+  def self.authenticate(username, password)
+    user = User.find_by_username(username)
+
+    return user if user.present? && user.password == password
+
+    nil
+  end
 end
