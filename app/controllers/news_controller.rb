f12519575b0d2d847ae8bae51ee8fcad210a4ae7
drapes <tihon1997@gmail.com>
2017-07-25 01:24:29 +0300

    Заполнил контроллеры News и Users

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,64 @@
 class NewsController < ApplicationController
+  before_action :load_news, except: [:index, :create]
+  before_action :authorize_user, only: [:create, :update, :destroy]
+
+  def index
+    @news_all = News.all
+
+    render json: @news_all
+  end
+
+  def show
+    render json: @news
+  end
+
+  def create
+    @new_news = News.new(news_params)
+    @new_news[:user_id] = current_user.id
+
+    if @new_news.save
+      render json: @new_news
+    else
+      render json: @new_news.errors
+    end
+  end
+
+  def update
+    if @news.update(news_params)
+      render json: @news
+    else
+      render json: @news.errors
+    end
+  end
+
+  def destroy
+    @news.destroy
+    raise @news.errors[:base].to_s unless @news.errors[:base].empty?
+    render json: {success: true}, status: 200
+  end
+
+  private
+
+  def news_params
+    news_params = {}
+    news_keys   = [:datetime, :сontent, :sources, :title]
+
+    news_keys.each do |key|
+      news_params[key] = params[key] if params[key]
+    end
+
+    news_params
+  end
+
+  def load_news
+    @news ||= News.find params[:id]
+  end
+
+  def authorize_user
+    # TODO
+  end
+
+  def current_user
+    # TODO
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,63 @@
 class UsersController < ApplicationController
+  before_action :load_user, except: [:create, :index]
+  before_action :authorize_user, except: [:create, :index]
+
+  def create
+    @new_user = User.new(user_params)
+
+    if @new_user.save
+      render json: @user
+    else
+      render json: @user.errors
+    end
+  end
+
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  def show
+    render json: @user
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors
+    end
+  end
+
+  def destroy
+    @user.destroy
+    raise @user.errors[:base].to_s unless @user.errors[:base].empty?
+    render json: {success: true}, status: 200
+  end
+
+  private
+
+  def user_params
+    user_params = {}
+    user_keys   = [:password, :username]
+
+    user_keys.each do |key|
+      user_params[key] = params[key] if params[key]
+    end
+
+    user_params
+  end
+
+  def authorize_user
+    # TODO
+  end
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
+  def current_user
+    # TODO
+  end
 end
