bf71a7332ece82ad197d070b69c69aa4cac78c93
drapes <tihon1997@gmail.com>
2017-07-25 00:49:07 +0300

    Создал контроллеры и прописал пути в routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :news
+  resources :users
+  resource :session, only: [:create, :destroy]
 end
