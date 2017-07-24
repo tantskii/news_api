c653e66d0b07b3545556a4e4c1876c9c6b52db8f
drapes <tihon1997@gmail.com>
2017-07-25 00:34:01 +0300

    Добавил зависимость между моделями User и News

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724211836) do
+ActiveRecord::Schema.define(version: 20170724213133) do
 
   create_table "news", force: :cascade do |t|
     t.date     "date"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170724211836) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_news_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
